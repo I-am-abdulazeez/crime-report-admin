10b1b5b4df47e6b323ba03cd9ebe8a89ef7ea5b6
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-17 18:51:38 +0100

    chore: import route provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
-import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import { theme } from './assets/theme';
 
@@ -9,8 +11,10 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
