039d0dced57f2b4980bb7f76be852d3cc4763c69
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-17 20:00:21 +0100

    chore: homepage mockup done

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,31 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Button, Center, Heading, Text } from '@chakra-ui/react';
+
+import { bgGradient, buttonGradient, overlayBg } from 'src/constants';
 
 const Home = () => {
   return (
-    <Box>
-      <Text>Home</Text>
-    </Box>
+    <Center
+      color={'white'}
+      bg={overlayBg}
+      height={'100vh'}
+      flexDir={'column'}
+      bgSize={'cover'}
+    >
+      <Heading fontSize={'2xl'} fontWeight={'black'}>
+        CrimeReport.
+      </Heading>
+      <Text my={'0.6rem'} fontSize={'lg'}>
+        Welcome to our Admin page.
+      </Text>
+      <Button
+        bgGradient={bgGradient}
+        _hover={buttonGradient}
+        _focus={buttonGradient}
+        color={'#000'}
+      >
+        Admin Login
+      </Button>
+    </Center>
   );
 };
 
