3445488ec3a00deaad4fb412ba53ab9bf7d55c80
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-17 21:42:33 +0100

    chore(ui): make logo/heading navigate to home. i.e '/'

--- src/components/CrimeHeading/CrimeHeading.tsx
+++ src/components/CrimeHeading/CrimeHeading.tsx
@@ -1,16 +1,24 @@
 import { Heading, Text } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 type CrimeHeadingProps = {
   text: string;
 };
 
 const CrimeHeading: React.FC<CrimeHeadingProps> = ({ text }) => {
+  const navigate = useNavigate();
   return (
     <>
-      <Heading fontSize={'2xl'} fontWeight={'black'}>
+      <Heading
+        onClick={() => navigate('/')}
+        cursor={'pointer'}
+        fontSize={'2xl'}
+        color={'white'}
+        fontWeight={'black'}
+      >
         CrimeReport.
       </Heading>
-      <Text my={'0.6rem'} fontSize={'lg'}>
+      <Text my={'0.6rem'} color={'white'} fontSize={'lg'}>
         {text}
       </Text>
     </>
