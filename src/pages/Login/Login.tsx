315a03fe29e93b1889af96b65370a4a414756048
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-17 20:24:55 +0100

    chore: Login mockup done

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,60 @@
-import { Box, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Text,
+} from '@chakra-ui/react';
+
+import { overlayBg, bgGradient, buttonGradient } from 'src/constants';
 
 const Login = () => {
   return (
-    <Box>
-      <Text>Login</Text>
-    </Box>
+    <Center bg={overlayBg} height={'100vh'} flexDir={'column'} bgSize={'cover'}>
+      <Heading color={'white'} fontSize={'2xl'} fontWeight={'black'}>
+        CrimeReport.
+      </Heading>
+      <Text color={'white'} my={'0.6rem'} fontSize={'lg'}>
+        Welcome, Admin
+      </Text>
+      <Container maxWidth={'350px'}>
+        <FormControl my={3}>
+          <FormLabel color={'white'}>Admin Email Address</FormLabel>
+          <Input
+            bgColor={'white'}
+            borderRadius={'md'}
+            type="email"
+            placeholder={'Email Address'}
+          />
+        </FormControl>
+        <FormControl my={3}>
+          <FormLabel color={'white'}>Admin Password</FormLabel>
+          <Input
+            bgColor={'white'}
+            borderRadius={'md'}
+            type="text"
+            placeholder={'Password'}
+          />
+        </FormControl>
+        <Box>
+          <Button
+            type={'submit'}
+            width={'full'}
+            mt={4}
+            bgGradient={bgGradient}
+            _hover={buttonGradient}
+            _focus={buttonGradient}
+            color={'#000'}
+          >
+            Login
+          </Button>
+        </Box>
+      </Container>
+    </Center>
   );
 };
 
