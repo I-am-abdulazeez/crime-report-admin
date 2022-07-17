1a208b68a70c772dfc3984dbb7ee995c5f94dcad
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-17 22:14:06 +0100

    chore: dashboard Ui composition

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,47 @@
-import { Box, Text } from '@chakra-ui/react';
+import {
+  Avatar,
+  Badge,
+  Box,
+  Button,
+  Container,
+  Heading,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spacer,
+  Text,
+} from '@chakra-ui/react';
 
 const Dashboard = () => {
   return (
-    <Box>
-      <Text>Dashboard</Text>
-    </Box>
+    <Container maxWidth={'container.lg'}>
+      <HStack py={4}>
+        <Heading fontSize={'xl'} fontWeight={'black'}>
+          CrimeReport.
+        </Heading>
+        <Badge colorScheme={'purple'} rounded={'lg'}>
+          Admin
+        </Badge>
+        <Spacer />
+        <Box>
+          <Menu isLazy arrowPadding={4} placement={'right-start'}>
+            <MenuButton>
+              <Avatar name="Crime Report" size={'sm'} />
+            </MenuButton>
+            <MenuList fontSize={'sm'}>
+              <MenuItem>Dark mode</MenuItem>
+              <MenuItem>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+      </HStack>
+
+      <Box mt={7}>
+
+      </Box>
+    </Container>
   );
 };
 
