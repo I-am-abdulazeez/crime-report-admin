26b3f0c77238c8c17d65acc0801790d693574936
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-18 10:20:39 +0100

    chore: moved all state in this component to store: zustand

--- src/components/AllCrimesTab/AllCrimesTab.tsx
+++ src/components/AllCrimesTab/AllCrimesTab.tsx
@@ -1,5 +1,4 @@
 import {
-  TabPanel,
   TableContainer,
   Table,
   TableCaption,
@@ -11,55 +10,52 @@ import {
   Tbody,
   Td,
 } from '@chakra-ui/react';
-import { DocumentData, QueryDocumentSnapshot } from 'firebase/firestore';
 
-type AllCrimesTabProps = {
-  isLoading: boolean;
-  crimes: QueryDocumentSnapshot<DocumentData>[];
-};
+import { useStore } from 'src/store';
 
-const AllCrimesTab: React.FC<AllCrimesTabProps> = ({ isLoading, crimes }) => {
+const AllCrimesTab = () => {
+  const { isLoadingUser, crimes } = useStore();
+
   return (
-      <TableContainer border={'1px solid #EDF2F7'} rounded={'lg'}>
-        <Table variant="striped">
-          <TableCaption>CrimeReport. Data</TableCaption>
-          {isLoading ? (
-            <Thead p={4}>
-              <Tr>
-                <Th>
-                  <Center>
-                    <Spinner emptyColor="gray.200" color="purple.500" />
-                  </Center>
-                </Th>
-              </Tr>
-            </Thead>
-          ) : (
-            <Thead>
-              <Tr>
-                <Th>Email Address</Th>
-                <Th>Name</Th>
-                <Th>Phone Number</Th>
-                <Th>Crime</Th>
-              </Tr>
-            </Thead>
-          )}
-          <Tbody>
-            {crimes &&
-              crimes.map((crime) => {
-                const id = crime.id;
-                return (
-                  <Tr key={id}>
-                    <Td>{crime.data().email}</Td>
-                    <Td>{crime.data().name}</Td>
-                    <Td>{crime.data().phoneNumber}</Td>
-                    <Td>{crime.data().crime}</Td>
-                  </Tr>
-                );
-              })}
-          </Tbody>
-        </Table>
-      </TableContainer>
-
+    <TableContainer border={'1px solid #EDF2F7'} rounded={'lg'}>
+      <Table variant="striped">
+        <TableCaption>CrimeReport. Data</TableCaption>
+        {isLoadingUser ? (
+          <Thead p={4}>
+            <Tr>
+              <Th>
+                <Center>
+                  <Spinner emptyColor="gray.200" color="purple.500" />
+                </Center>
+              </Th>
+            </Tr>
+          </Thead>
+        ) : (
+          <Thead>
+            <Tr>
+              <Th>Email Address</Th>
+              <Th>Name</Th>
+              <Th>Phone Number</Th>
+              <Th>Crime</Th>
+            </Tr>
+          </Thead>
+        )}
+        <Tbody>
+          {crimes &&
+            crimes.map((crime) => {
+              const id = crime.id;
+              return (
+                <Tr key={id}>
+                  <Td>{crime.data().email}</Td>
+                  <Td>{crime.data().name}</Td>
+                  <Td>{crime.data().phoneNumber}</Td>
+                  <Td>{crime.data().crime}</Td>
+                </Tr>
+              );
+            })}
+        </Tbody>
+      </Table>
+    </TableContainer>
   );
 };
 
