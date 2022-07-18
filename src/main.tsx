09e726925def4a9c34d34cbeca0d2f47d4058da2
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-18 10:10:24 +0100

    chore: making history available outside react-components

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { BrowserRouter } from 'react-router-dom';
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
 import { ChakraProvider } from '@chakra-ui/react';
 
 import { theme } from './assets/theme';
+
+export const history = createBrowserHistory({ window });
 
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HistoryRouter history={history}>
       <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
-    </BrowserRouter>
+    </HistoryRouter>
   </React.StrictMode>
 );
