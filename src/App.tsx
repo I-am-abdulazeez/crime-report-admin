27a25da48c790efc966f513676b6c71c329b5408
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-18 10:20:07 +0100

    chore: moved all global state to store: zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-
-import {
-  DocumentData,
-  QueryDocumentSnapshot,
-  collection,
-  query,
-  getDocs,
-} from 'firebase/firestore';
 
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Dashboard from './pages/Dashboard/Dashboard';
 
-import { firestoreDb } from './libs';
+import { useStore } from './store';
 
 const App = () => {
-  const [crimes, setCrimes] = useState<QueryDocumentSnapshot<DocumentData>[]>(
-    []
-  );
-  const [isLoading, setIsLoading] = useState(true);
-
-  const fetchCrimes = async () => {
-    const crimeQuery = query(collection(firestoreDb, 'crimes'));
-    setIsLoading(true);
-    await getDocs(crimeQuery)
-      .then((snapshot) => {
-        setCrimes(snapshot?.docs);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        console.log(err);
-      });
-  };
+  const { fetchCrimes } = useStore();
 
   useEffect(() => {
     fetchCrimes();
@@ -43,10 +18,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route
-        path="/dashboard"
-        element={<Dashboard crimes={crimes} isLoading={isLoading} />}
-      />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 };
